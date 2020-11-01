ad9bd7c494fe7874543370ad55ed6150b654c3c0
Toivo Wuoti <toivo.wuoti@tuni.fi>
2020-11-01 18:15:43 +0200

    Piirtää kuvan

--- Game/main.cc
+++ Game/main.cc
@@ -1,9 +1,20 @@
+#include "../CourseLib/graphics/simplemainwindow.hh"
+
 #include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Q_INIT_RESOURCE(offlinedata);
+
+    QImage kuva;
+    kuva.load(":/offlinedata/offlinedata/kartta_iso_1095x592.png");
+
+    CourseSide::SimpleMainWindow ikkuna;
+    ikkuna.setPicture(kuva);
+
+    ikkuna.show();
 
     return a.exec();
 }
